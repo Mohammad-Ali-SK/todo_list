45bfd9dab619b7364d4f6c9d990b491ebfcab64d
Mohammad Ali sk <mohammadalisk050@gmail.com>
2024-02-07 03:41:04 +0530

    2/7/24

--- app/page.js
+++ app/page.js
@@ -2,62 +2,61 @@
 import React, { useState } from 'react'
 
 const page = () => {
-const [title, settitle] = useState('');
-const [desc, setdesc] = useState('');
-const [maintask, setmaintask] = useState('');
+  const [title, settitle] = useState('');
+  const [desc, setdesc] = useState('');
+  const [maintask, setmaintask] = useState('');
 
-let submitHandler = (e) => {
-  e.preventDefault();
-  setmaintask([...maintask,{title,desc}]);
-  setdesc('');
-  settitle('');
-}
+  let submitHandler = (e) => {
+    e.preventDefault();
+    setmaintask([...maintask,{title,desc}]);
+    settitle('');
+    setdesc('');
+  };
+  let rendertask = <h2>No task here</h2>;
+  if(maintask.length > 0){
+    rendertask = maintask.map((e,i) => {
+      return(
+        <li key={i} className=' flex items-center justify-between text-center mb-4'>
+          <div className=' flex items-center justify-between w-[40%]'>
+            <h5>{e.title}</h5>
+            <h6>{e.desc}</h6>
+          </div>
+          <button onClick={ () => {
+            deleteHandler()
+          }} className=' bg-red-600 py-3 px-4 text-white font-bold  rounded'>Delete</button>
+        </li>
+      )
+    })
+  };
+
 let deleteHandler = (i) => {
   let copytext = [...maintask];
   copytext.splice(i,1);
   setmaintask(copytext);
-  console.log(i)
-
 }
 
-let rendertask = <h2>No task are avalable here</h2>;
-if(maintask.length > 0){
-  rendertask = maintask.map((e,i) => {
-    return <li key={i} className=' mb-3 flex items-center justify-between'>
-      <div className=' flex items-center w-[40%] justify-between '>
-        <h5>{e.title}</h5>
-        <h6>{e.desc}</h6>
-      </div>
-      <button onClick={() => {
-        deleteHandler();
-      }}  className=' bg-red-600 text-white text-bold py-3 px-5 rounded'>Delete</button>
-    </li>
-  })
-}
 
 
   return (
-    <>
-      <h1 className=' bg-black text-white text-bold text-3xl py-3 text-center'>My Todo list</h1>
-    
-      <form onSubmit={submitHandler}>
-        <input type='text' placeholder='Enter task here' className=' m-6 border-black border-2 py-3 px-4 rounded-[6px]'  value={title} onChange={(e) => {
+    <div>
+      <h2 className=' w-full py-6 bg-black text-white text-3xl font-bold text-center'>My To do list</h2>
+      <form onSubmit = {submitHandler}>
+        <input type='text' placeholder='Enter your task here ' className=' border-2 border-black py-3 px-5 m-4 rounded' value={title} onChange={(e) => {
           settitle(e.target.value);
         }}/>
-        <input type='text' placeholder='Enter description here' className=' m-6 border-black border-2 py-3 px-4 rounded-[6px]' value={desc} onChange={(e) => {
+        <input type='text' placeholder='Enter your description here' className=' border-2 border-black py-3 px-5 m-4 rounded' value={desc} onChange={(e) => {
           setdesc(e.target.value)
         }}/>
-      <button className='bg-black text-white py-3 px-5 rounded'>Add task</button>
+        <button  className=' bg-black text-white font-bold py-3 px-4 rounded'>Add task</button>
+        <br/>
+        <hr/>
       </form>
-      <br/>
-      <hr/>
-      <br/>
       <div className=' bg-slate-400 p-8'>
         <ul>
           {rendertask}
         </ul>
       </div>
-    </>
+    </div>
   )
 }
 
